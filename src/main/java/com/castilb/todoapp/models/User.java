0fdd615e7c8f42fe9f5af7bf207f07d4b915ca36
icastillo <icastillo@cevaldom.com>
2024-01-09 17:20:30 -0400

    User model crated

---
+++ src/main/java/com/castilb/todoapp/models/User.java
@@ -0,0 +1,22 @@
+package com.castilb.todoapp.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+}
