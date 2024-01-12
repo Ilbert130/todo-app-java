1c349d96142e2d0e2f3e52b4e7010b6b5d33e199
icastillo <icastillo@cevaldom.com>
2024-01-11 21:31:56 -0400

    proyect repository created

--- src/main/java/com/castilb/todoapp/repositories/ProyectRepository.java
+++ src/main/java/com/castilb/todoapp/repositories/ProyectRepository.java
@@ -1,9 +1,9 @@
 package com.castilb.todoapp.repositories;
 
-import com.castilb.todoapp.models.List;
+import com.castilb.todoapp.models.Proyect;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ListRepository extends JpaRepository<List, Integer> {
+public interface ProyectRepository extends JpaRepository<Proyect, Integer> {
 }
